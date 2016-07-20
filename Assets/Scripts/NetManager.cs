5b0a449e37f894ad7f3a0cfcc2dffe9854cf5314
lujian <lujian@lujiandeMacBook-Pro.local>
2016-07-20 18:56:47 +0800

    Add Scripts, ThirdParty

---
+++ Assets/Scripts/NetManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UGFramework
+{
+	/**
+	 * --- DOC BEGIN ---
+	 * Network manager, manage request, response, dispatch net protocols
+	 * Supports TCP, UDP, HTTP
+	 * --- DOC END ---
+	 */ 
+	public class NetManager : MonoBehaviour 
+	{
+	}
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UGFramework
+{
+	public class ResourceManager : MonoBehaviour 
+	{
+	}
+}
