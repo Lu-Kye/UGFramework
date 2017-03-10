b2a48674f271939b3db7de376036f7a16362de16
lukye <lujian0517@163.com>
2017-03-10 11:37:34 +0800

    Add asset usage detector

---
+++ Assets/UGEditor/EditorMenu/SidebarConfig.cs
@@ -0,0 +1,14 @@
+namespace UGFramework.EditorMenu
+{
+	/**
+	 * --- DOC BEGIN ---
+     * Custom sidebar menu configuration
+	 * --- DOC END ---
+	 */ 
+    public class TopbarConfig
+    {
+        public const string ROOT = "UGFramework";
+        public const string EXAMPLES = ROOT + "/Examples";
+        public const string THIRDPARTY = ROOT + "/ThirdParty";
+    }
+}
--- Documents/TODO.md
+++ Documents/TODO.md
@@ -1,3 +1,5 @@
 * View coroutine status in *Inspector*
 * Object pool finish and optimize
 * Class document generator 
+* Find asset using reference editor
+* Find object reference at runtime
